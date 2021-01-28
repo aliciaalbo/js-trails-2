e59d113b33dc8abf6039e72341c912db7006d0e7
Alicia Albo <aliciaalbo@gmail.com>
2021-01-28 20:05:59 +0000

    solve arrays.js

--- arrays.js
+++ arrays.js
@@ -3,17 +3,29 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for(let i = 0; i<items.length; i++){
+    console.log($`{items[i]} {i}`);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const i in items){
+    if( i % 2 === 0){
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = [];
+  sortedItems = items.sort((a,b) => a-b).slice(0,n);
+  sortedItems.reverse();
+  console.log(sortedItems);
 }
